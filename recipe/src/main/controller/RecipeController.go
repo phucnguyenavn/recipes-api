32a41829bb14f0a04dac36500ae4cd857ba86f6f
Phuc Nguyen Thanh <nguyenphuc.vna@gmail.com>
2022-01-19 10:50:46 +0700

    update recipe endpoint

--- recipe/src/main/controller/RecipeController.go
+++ recipe/src/main/controller/RecipeController.go
@@ -38,6 +38,48 @@ func ListRecipesHandler(c *gin.Context) {
 
 }
 
+func UpdateRecipeHandler(c *gin.Context) {
+	var recipe model.Recipe
+	id := c.Param("id")
+	err := c.ShouldBindJSON(&recipe)
+	if err != nil {
+
+		c.JSON(http.StatusBadRequest, gin.H{
+
+			"error": err.Error()})
+
+		return
+
+	}
+
+	index := -1
+
+	for i := 0; i < len(recipes); i++ {
+
+		if recipes[i].ID == id {
+
+			index = i
+
+		}
+
+	}
+
+	if index == -1 {
+
+		c.JSON(http.StatusNotFound, gin.H{
+
+			"error": "Recipe not found"})
+
+		return
+
+	}
+
+	recipes[index] = recipe
+
+	c.JSON(http.StatusOK, recipe)
+
+}
+
 var recipes []model.Recipe
 
 func init() {
