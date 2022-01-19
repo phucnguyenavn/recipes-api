2a400df5a8006c5f3ad458fbb79f303cf5a30324
Phuc Nguyen Thanh <nguyenphuc.vna@gmail.com>
2022-01-19 11:00:10 +0700

    delete recipe endpoint

--- recipe/src/main/controller/RecipeController.go
+++ recipe/src/main/controller/RecipeController.go
@@ -80,6 +80,40 @@ func UpdateRecipeHandler(c *gin.Context) {
 
 }
 
+func DeleteRecipeHandler(c *gin.Context) {
+
+	id := c.Param("id")
+
+	index := -1
+
+	for i := 0; i < len(recipes); i++ {
+
+		if recipes[i].ID == id {
+
+			index = i
+
+		}
+
+	}
+
+	if index == -1 {
+
+		c.JSON(http.StatusNotFound, gin.H{
+
+			"error": "Recipe not found"})
+
+		return
+
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...)
+
+	c.JSON(http.StatusOK, gin.H{
+
+		"message": "Recipe has been deleted",
+	})
+}
+
 var recipes []model.Recipe
 
 func init() {
--- recipe/src/main/main.go
+++ recipe/src/main/main.go
@@ -11,5 +11,6 @@ func main() {
 	router.POST("/recipes", controller.NewRecipeHandler)
 	router.GET("/recipes", controller.ListRecipesHandler)
 	router.PUT("/recipes/:id", controller.UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", controller.DeleteRecipeHandler)
 	router.Run()
 }
